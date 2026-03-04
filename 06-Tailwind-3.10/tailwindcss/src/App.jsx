41bc4f6748951ac7f0b955064d6b9225a4bf5406
Santosh Padhiali <santoshpadhiali007@gmail.com>
2026-03-05 01:13:33 +0530

    Connect Tailwind CSS Succfully

---
+++ 06-Tailwind-3.10/tailwindcss/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-pink-800'>Hay i am tailwind css</h1>
+      <h1 class="text-3xl font-bold underline  text-yellow-300">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
