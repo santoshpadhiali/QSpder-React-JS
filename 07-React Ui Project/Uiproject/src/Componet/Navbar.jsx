f514eee9af2ddeaf459f9c2adbf1737464bfde4f
Santosh Padhiali <santoshpadhiali007@gmail.com>
2026-03-07 20:58:58 +0530

    it start

---
+++ 07-React Ui Project/Uiproject/src/Componet/Navbar.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+
+function Navbar(){
+    return (
+        <>
+
+        <div className=' text-amber-900 '> Navbar</div>
+        <h1 className=''>Navbar</h1>
+
+        </>
+    )
+}
+export default Navbar;
