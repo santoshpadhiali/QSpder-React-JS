e088ae6dee986785ea329bff6c7b84f8d52b9985
Santosh Padhiali <santoshpadhiali007@gmail.com>
2026-03-18 23:53:31 +0530

    App change

--- 08_Warking_With_Fuction 4.30/WorkFunction/src/App.jsx
+++ 08_Warking_With_Fuction 4.30/WorkFunction/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function App() {
+  function handleClick(){
+    console.log('btn is clicked ');
+  }
   return (
     <div>
-      
+      <button onClick={handleClick}> btnClick</button>
+
+      <input type="text"  placeholder='type somethiong' onChange={function(e){
+        console.log(e.target.value);
+        
+      }}/>
     </div>
   )
 }
