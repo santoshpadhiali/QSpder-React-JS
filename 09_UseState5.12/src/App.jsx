526c91b8ba4fe260ba7a879b5f5f1a7b1481a632
Santosh Padhiali <santoshpadhiali007@gmail.com>
2026-03-20 00:16:58 +0530

    Simply Counter Use by Use State

--- 09_UseState5.12/src/App.jsx
+++ 09_UseState5.12/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+let[count,setCount]=  useState(0)
+function inc(){
+  setCount(perv => perv+1);
+}
+function Dec(){
+  setCount(perv => perv >0 ? perv-1 : 0);
+}
   return (
     <div>
-      
+      <h1>Count Value : {count}</h1>
+      <button onClick={inc}>➕➕</button>
+      <button onClick={Dec}>--</button>
     </div>
   )
 }
