164bf22f757ae0672d39feccd59f49426723bad7
Santosh Padhiali <santoshpadhiali007@gmail.com>
2026-03-19 23:47:42 +0530

    CONTINUE

--- 08_Warking_With_Fuction 4.30/WorkFunction/src/App.jsx
+++ 08_Warking_With_Fuction 4.30/WorkFunction/src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
 function App() {
-  function handleClick(){
-    console.log('btn is clicked ');
+  function handleClick(e) {
+    console.log();
+  }
+  function scroll(e){
+    console.log(e.deltaY);
+    
   }
   return (
     <div>
-      <button onClick={handleClick}> btnClick</button>
+
+      {/* <button onClick={handleClick}> btnClick</button>
 
       <input type="text"  placeholder='type somethiong' onChange={function(e){
         console.log(e.target.value);
         
-      }}/>
+      }}/> */}
+      <div className="par" onWheel={scroll}>
+
+        <div className="page-1">hay</div>
+        <div className="page-2">love</div>
+        <div className="page-3">how </div>
+
+      </div>
     </div>
   )
 }
